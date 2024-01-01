b5f10c05f30d765f1750d5bc6e5d03a69e1d6975
KozerT <kozer.vizastaff@gmail.com>
2024-01-01 22:00:12 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Tic-Tac-Toe: The Ultimate Challenge</h1>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player1</span>
+            <span className="player-name">Player2</span>
+          </li>
+          <li></li>
+        </ol>
+      </div>
+      LOG
+    </>
+  );
+}
+
+export default App;
