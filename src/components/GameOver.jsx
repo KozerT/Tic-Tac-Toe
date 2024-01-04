586685475f0231d53d05f0bf32ce0bc9cf3bdde4
KozerT <kozer.vizastaff@gmail.com>
2024-01-04 14:51:11 +0100

    style: Added styles for the GameOver component

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const GameOver = ({ winner }) => {
   return (
-    <div id="game-over">
-      <h2>Game Over!</h2>
-      {winner && <p>{winner} won!</p>}
-      {!winner && <p>It&apos;s a draw!</p>}
+    <div className="game-over">
+      <h2 className="game-over__text__main">Game Over!</h2>
+      {winner && <p className="game-over__text__paragraph">{winner} won!</p>}
+      {!winner && (
+        <p className="game-over__text__paragraph">It&apos;s a draw!</p>
+      )}
       <p>
-        <button>Restart</button>
+        <button className="game-over__button">Restart</button>
       </p>
     </div>
   );
